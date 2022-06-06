8a350aabd8e9ea4d6233ae6c9a85b276786d89cd
Joymania <joymania_pust@outlook.com>
2022-06-06 21:08:47 +0600

    overall api consume profile & upload prescription

--- src/components/Profile/OrderCard/OrderCard.jsx
+++ src/components/Profile/OrderCard/OrderCard.jsx
@@ -1,67 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Link } from "react-router-dom";
+import AttachPrescriptionAlert from "../PrescriptionAlert/AttachPrescriptionAlert";
+import UploadPrescriptionAlert from "../PrescriptionAlert/UploadPrescriptionAlert";
 import {
   urlOrderDetailsRoute,
   urlProfileRoute,
 } from "../../../Service/UrlService";
 
 const OrderCard = ({ order }) => {
+  console.log({ order });
   let created_at = new Date(order.CreatedAt);
   let history = useHistory();
-
+  const [UploadPresAlert, setUploadPresAlert] = useState(false);
+  const [attachPres, setAttachPres] = useState(false);
+  const closePrescriptionModal = () => {
+    setUploadPresAlert((prevState) => !prevState);
+  };
+  const closeAttachPreshandler = () => {
+    setAttachPres((prevState) => !prevState);
+  };
+  const uploadPrescriptionHandler = () => {
+    setUploadPresAlert((prevState) => !prevState);
+  };
+  const attachedPrescriptionHandler = () => {
+    setAttachPres((prevState) => !prevState);
+  };
   const clickedToViewOrder = () => {
     history.push(urlProfileRoute() + urlOrderDetailsRoute() + order.Id);
   };
   return (
-    <div id="Tab6" class="tabcontent tab-content detalis-page-tab-content">
-      {/* <!-- product desc review information --> */}
-      <div class="profile-order-tab">
-        <a href>
-          <div class="order-id display-flex jc-s-b">
-            <span> Order ID #{order.OrderNo}</span>
-            <ul className="group-of-buttons">
-              <li>
-                <div className="button-order">
-                  <div>Upload Prescription </div>
-                </div>
-              </li>
-              <li>
-                <div className="button-order">
-                  <div>Attach Prescription </div>
-                </div>
-              </li>
-              <li>
-                <div className="button-order" onClick={clickedToViewOrder}>
-                  <div>View Details</div>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div class="order-dsc display-flex jc-s-b">
-            <span> Date</span>
-            <aside>12 October'22 02:30 PM</aside>
-          </div>
-          <div class="order-dsc display-flex jc-s-b">
-            <span> Payable Amount</span>
-            <aside>{order.PayableAmount} tk</aside>
-          </div>
-          <div class="order-dsc display-flex jc-s-b">
-            <span>Shipping Address</span>
-            <aside>
-              {order.Remarks} {order.Upazila} {order.District} {order.Province}
-            </aside>
-          </div>
-          {/* <p>
+    <>
+      <div id="Tab6" class="tabcontent tab-content detalis-page-tab-content">
+        {/* <!-- product desc review information --> */}
+        <div class="profile-order-tab">
+          <a href>
+            <div class="order-id display-flex jc-s-b">
+              <span> Order ID #{order.OrderNo}</span>
+              <ul className="group-of-buttons">
+                <li>
+                  <div
+                    className="button-order"
+                    onClick={uploadPrescriptionHandler}
+                  >
+                    <div>Upload Prescription </div>
+                  </div>
+                </li>
+                <li>
+                  <div
+                    className="button-order"
+                    onClick={attachedPrescriptionHandler}
+                  >
+                    <div>Attach Prescription </div>
+                  </div>
+                </li>
+                <li>
+                  <div className="button-order" onClick={clickedToViewOrder}>
+                    <div>View Details</div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="order-dsc display-flex jc-s-b">
+              <span> Date</span>
+              <aside>12 October'22 02:30 PM</aside>
+            </div>
+            <div class="order-dsc display-flex jc-s-b">
+              <span> Payable Amount</span>
+              <aside>{order.PayableAmount} tk</aside>
+            </div>
+            <div class="order-dsc display-flex jc-s-b">
+              <span>Shipping Address</span>
+              <aside>
+                {order.Remarks} {order.Upazila} {order.District}{" "}
+                {order.Province}
+              </aside>
+            </div>
+            {/* <p>
             Lorem ipsum dolor sit amet consectetur , adipisicing elit.
             Necessitatibus fugit, soluta dicta obcaecati eos voluptatibus iste
             culpa, provident est nihil voluptas exercitationem possimus, a
             suscipit?
           </p> */}
-        </a>
+          </a>
+        </div>
+        {/* <!-- product desc review information --> */}
       </div>
-      {/* <!-- product desc review information --> */}
-    </div>
+      {UploadPresAlert && (
+        <UploadPrescriptionAlert
+          orderNo={order.Id}
+          closeModal={closePrescriptionModal}
+        />
+      )}
+      {attachPres && (
+        <AttachPrescriptionAlert
+          closeModal={closeAttachPreshandler}
+          orderId={order.Id}
+        />
+      )}
+    </>
   );
 };
 
--- src/components/Profile/RequestHistory/RequestHistoryAlert.jsx/RequestHistoryAlert.jsx
+++ src/components/Profile/RequestHistory/RequestHistoryAlert.jsx/RequestHistoryAlert.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { BASE_URL } from "../../../../Service/httpService";
 
-const RequestHistoryAlert = ({closeRequestAlert}) => {
+const RequestHistoryAlert = ({
+  closeRequestAlert,
+  requestItems,
+  requestPres,
+}) => {
   return (
     <div id="pop-up">
       <div class="overlay__popup show">
@@ -9,32 +14,37 @@ const RequestHistoryAlert = ({closeRequestAlert}) => {
             <h2>Order Placed Successfully</h2>
             <div
               style={{ color: "white", fontSize: "1.5rem", cursor: "pointer" }}
-              onClick = {closeRequestAlert}
+              onClick={closeRequestAlert}
             >
               ✖
             </div>
           </div>
           <div class="popup__body">
             <div className="image-founded-container">
-              {/* <p className="image-container__no-image" >No Image Found!</p> */}
               <p className="image-container__no-image">
                 Image of your prescription!
               </p>
-
-              <div className="image_preview_container">
-                <div className="image_previewer">
-                  <div className="image_prev">
-                    <img
-                      src="/Contents/assets/image/banner2.jpg"
-                      alt="img"
-                      srcset=""
-                    />
-                  </div>
+              {requestPres.length === 0 && (
+                <p className="image-container__no-image">No Image Found!</p>
+              )}
+              {requestPres.length > 0 && (
+                <div className="image_preview_container">
+                  {requestPres.map((item) => (
+                    <div className="image_previewer">
+                      <div className="image_prev">
+                        <img
+                          src={BASE_URL+item.FilePath}
+                          alt="img"
+                          srcset=""
+                        />
+                      </div>
+                    </div>
+                  ))}
                 </div>
-              </div>
+              )}
             </div>
-            <p className="item_found">5 items found</p>
-            
+            <p className="item_found">{requestItems.length} items found</p>
+
             <div className="items-found-container">
               <div className="items_founded_table_item">
                 <div className="table_header_items">
@@ -43,52 +53,13 @@ const RequestHistoryAlert = ({closeRequestAlert}) => {
                   <div className="header_quantity">Quantity</div>
                 </div>
                 <div className="table_content_items">
-                  <div className="table_content__single-item">
-                    <div className="content_name">Napa</div>
-                    <div className="content_strength">500</div>
-                    <div className="content_quantity">100</div>
-                  </div>
-                  <div className="table_content__single-item">
-                    <div className="content_name">Napa</div>
-                    <div className="content_strength">500</div>
-                    <div className="content_quantity">100</div>
-                  </div>
-                  <div className="table_content__single-item">
-                    <div className="content_name">Napa</div>
-                    <div className="content_strength">500</div>
-                    <div className="content_quantity">100</div>
-                  </div>
-                  <div className="table_content__single-item">
-                    <div className="content_name">Napa</div>
-                    <div className="content_strength">500</div>
-                    <div className="content_quantity">100</div>
-                  </div>
-                  <div className="table_content__single-item">
-                    <div className="content_name">Napa</div>
-                    <div className="content_strength">500</div>
-                    <div className="content_quantity">100</div>
-                  </div>
-                  <div className="table_content__single-item">
-                    <div className="content_name">Napa</div>
-                    <div className="content_strength">500</div>
-                    <div className="content_quantity">100</div>
-                  </div>
-                  <div className="table_content__single-item">
-                    <div className="content_name">Napa</div>
-                    <div className="content_strength">500</div>
-                    <div className="content_quantity">100</div>
-                  </div>
-                  <div className="table_content__single-item">
-                    <div className="content_name">Napa</div>
-                    <div className="content_strength">500</div>
-                    <div className="content_quantity">100</div>
-                  </div>
-                  <div className="table_content__single-item">
-                    <div className="content_name">Napa</div>
-                    <div className="content_strength">500</div>
-                    <div className="content_quantity">100</div>
-                  </div>
-
+                  {requestItems.map((item) => (
+                    <div className="table_content__single-item">
+                      <div className="content_name">{item.Name}</div>
+                      <div className="content_strength">{item.Strength}</div>
+                      <div className="content_quantity">{item.Quantity}</div>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
